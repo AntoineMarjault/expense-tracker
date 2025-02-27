1e314602c89de890571ef064ec113d3bd7fefbaa
Antoine Marjault <ant-mar-du37@hotmail.fr>
2025-02-27 15:52:30 +0100

    feat: converted amount to number and hide udpated_at and created_at from the JSON response

--- backend/app/models/transaction.rb
+++ backend/app/models/transaction.rb
@@ -2,4 +2,10 @@ class Transaction < ApplicationRecord
     belongs_to :category
 
     validates :amount, presence: true
+
+    def as_json(options = {})
+        super(only: [:id, :amount, :name, :date, :currency, :category_id]).tap do |hash|
+          hash['amount'] = amount.to_f if amount.present?
+        end
+    end
 end
