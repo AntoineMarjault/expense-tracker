2e4dcb6541777c4ff6512fb72f24e1d6a70e9a16
Antoine Marjault <ant-mar-du37@hotmail.fr>
2025-03-06 11:03:38 +0100

    chore: refactor layouts

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,43 +1,25 @@
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
-import '../globals.css'
-import Nav from '@/app/components/Nav'
-import Header from '@/app/components/Header'
+import Nav from '@/components/Nav'
+import Header from '@/components/Header'
 import { ReactNode } from 'react'
 import { QueryProvider } from '@/app/providers/QueryProvider'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Dashboard - Expense Tracker',
 }
 
-export default function RootLayout({
+export default function DashboardLayout({
   children,
 }: Readonly<{
   children: ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <QueryProvider>
-          <div className="flex flex-col min-h-screen bg-gray-100">
-            <Header />
-            <main className="flex-1 overflow-auto px-4 py-2">{children}</main>
-            <Nav className="sticky bottom-0" />
-          </div>
-        </QueryProvider>
-      </body>
-    </html>
+    <QueryProvider>
+      <div className="flex flex-col min-h-screen bg-gray-100">
+        <Header />
+        <main className="flex-1 overflow-auto px-4 py-2">{children}</main>
+        <Nav className="sticky bottom-0" />
+      </div>
+    </QueryProvider>
   )
 }
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,18 @@ import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
 import { ReactNode } from 'react'
 import { authOptions } from '@/lib/auth'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export default async function RootLayout({
   children,
@@ -14,5 +26,13 @@ export default async function RootLayout({
     redirect('/signin')
   }
 
-  return <>{children}</>
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  )
 }
