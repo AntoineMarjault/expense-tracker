7a1a1c7dcb73cd3d6acefe981c6a22c02609dc49
Antoine Marjault <ant-mar-du37@hotmail.fr>
2025-03-29 18:04:18 +0100

    feat: do not display the floating add button on the budget details page

--- frontend/app/dashboard/budgets/layout.tsx
+++ frontend/app/dashboard/budgets/layout.tsx
@@ -1,11 +1,17 @@
+'use client'
+
 import { ReactNode } from 'react'
 import FloatingActionButton from '@/components/ui/custom/FloatingActionButton'
+import { usePathname } from 'next/navigation'
 
 export default function BudgetsLayout({ children }: { children: ReactNode }) {
+  const pathname = usePathname()
+  const isListPage = pathname === '/dashboard/budgets'
+
   return (
     <>
       {children}
-      <FloatingActionButton href="/dashboard/budgets/new" />
+      {isListPage && <FloatingActionButton href="/dashboard/budgets/new" />}
     </>
   )
 }
