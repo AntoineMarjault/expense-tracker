76c7d5d90061b731e66777185b9ed42ad99b2c99
Antoine Marjault <ant-mar-du37@hotmail.fr>
2025-04-25 21:05:40 +0200

    fix: fix migration on prod db second try

--- backend/db/migrate/20250425174813_simplify_location_to_country.rb
+++ backend/db/migrate/20250425174813_simplify_location_to_country.rb
@@ -1,22 +1,16 @@
 class SimplifyLocationToCountry < ActiveRecord::Migration[8.0]
   def up
-    remove_foreign_key :transactions, :locations if foreign_key_exists?(:transactions, :locations)
-
-    remove_reference :transactions, :location if column_exists?(:transactions, :location_id)
-
     add_column :transactions, :country_code, :string, limit: 2 unless column_exists?(:transactions, :country_code)
+
+    execute "UPDATE transactions SET location_id = NULL"
+
+    remove_foreign_key :transactions, :locations if foreign_key_exists?(:transactions, :locations)
+    remove_reference :transactions, :location if column_exists?(:transactions, :location_id)
 
     drop_table :locations if table_exists?(:locations)
   end
 
   def down
-    create_table :locations do |t|
-      t.string :country_code, limit: 2, null: false
-      t.timestamps
-    end
-
-    remove_column :transactions, :country_code if column_exists?(:transactions, :country_code)
-
-    add_reference :transactions, :location, foreign_key: true
+    raise ActiveRecord::IrreversibleMigration, "Can't restore locations data"
   end
 end
