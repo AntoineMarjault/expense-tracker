50a249f3caa553037fa29df6af740b91f1e8efe6
Antoine Marjault <ant-mar-du37@hotmail.fr>
2025-05-12 16:02:16 +0200

    fix: fix production and test suite, set up the scafholder for currency conversion

--- backend/app/models/transaction.rb
+++ backend/app/models/transaction.rb
@@ -3,11 +3,31 @@ class Transaction < ApplicationRecord
   belongs_to :category
 
   validates :amount, presence: true
+  validates :amount_in_default_currency, presence: true
   validates :country_code, length: { is: 2 }, allow_nil: true
+
+  before_validation :set_amount_in_default_currency, if: :should_convert_amount?
 
   def as_json(options = {})
     super(only: [ :id, :amount, :name, :date, :currency, :category_id, :country_code ]).tap do |hash|
       hash["amount"] = amount.to_f if amount.present?
+    end
+  end
+
+  private
+
+  def should_convert_amount?
+    amount.present? && currency.present? &&
+    (amount_in_default_currency.nil? || amount_changed? || currency_changed?)
+  end
+
+  def set_amount_in_default_currency
+    # Here you would use a currency conversion service
+    self.amount_in_default_currency = if currency == user.default_currency
+      amount
+    else
+      # Implement actual conversion using user.default_currency as target
+      amount # placeholder
     end
   end
 end
--- backend/spec/factories/transaction.rb
+++ backend/spec/factories/transaction.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
     factory :transaction do
         sequence(:name) { |n| "Transaction #{n}" }
         amount { 10 }
+        amount_in_default_currency { 10 }
         currency { 'EUR' }
         association :category
         date { Time.now }
